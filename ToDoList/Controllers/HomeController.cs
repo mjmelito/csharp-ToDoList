4faed8b4b6399fd8de754c4cbb62c69ca2a281a6
Matt Melito <matt_melito@outlook.com>
2023-05-10 06:50:28 -0700

    Add ToDoList directory and ToDoLsit.Tests directory with all logic and tests for category and item classes.

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+ 
+
+    }
+}
